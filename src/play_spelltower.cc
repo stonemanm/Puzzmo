a66443a03c0446e4f7a64c1364e018912771d6ca
Michael Stoneman <michaeljohnstoneman+github@gmail.com>
2025-03-10 05:43:55 -0400

    Refactoring

--- src/play_spelltower.cc
+++ src/play_spelltower.cc
@@ -14,6 +14,16 @@ using namespace puzzmo;
 using ::spelltower::Path;
 using ::spelltower::Solver;
 
+//-------
+// Files
+
+ABSL_FLAG(std::string, spelltower_board_file_path, "data/spelltower_board.txt",
+          "Path to the input file containing a string representation of the "
+          "board, with star letters capitalized.");
+
+//------------
+// Parameters
+
 ABSL_FLAG(bool, print_current_options, false,
           "Print all playable words and their scores to the command line.");
 
@@ -28,14 +38,15 @@ ABSL_FLAG(bool, solve_greedily, false,
 
 namespace {
 
-constexpr absl::string_view kSpelltowerBoardFilePath =
-    "data/spelltower_board.txt";
+//---------
+// Modules
 
 absl::StatusOr<Solver> LoadSolver() {
-  std::ifstream file(kSpelltowerBoardFilePath);
+  std::ifstream file(absl::GetFlag(FLAGS_spelltower_board_file_path));
   if (!file.is_open()) {
     return absl::InvalidArgumentError(
-        absl::StrCat("Error: Could not open ", kSpelltowerBoardFilePath));
+        absl::StrCat("Error: Could not open ",
+                     absl::GetFlag(FLAGS_spelltower_board_file_path)));
   }
   std::vector<std::string> grid_strings;
   std::string line;
@@ -46,7 +57,36 @@ absl::StatusOr<Solver> LoadSolver() {
   return Solver::CreateSolverWithSerializedDict(grid_strings);
 }
 
+void PrintCurrentOptions(const Solver &solver) {
+  absl::btree_map<int, absl::btree_set<Path>, std::greater<int>> wc =
+      solver.word_cache();
+  LOG(INFO) << "All possible words on grid: ";
+  for (const auto &[score, wds] : wc) {
+    LOG(INFO) << absl::StrCat(
+        score, ": ",
+        absl::StrJoin(wds, ", ", [](std::string *out, const Path &path) {
+          absl::StrAppend(out, path.word());
+        }));
+  }
+}
+
+absl::Status PrintLongestAllStarWord(const Solver &solver) {
+  absl::StatusOr<Path> path = solver.LongestPossibleAllStarWord();
+  if (!path.ok()) return path.status();
+
+  LOG(INFO) << absl::StrCat("Longest possible all-star word: ", path->word());
+  for (int i = 0; i < path->size(); ++i) {
+    LOG(INFO) << absl::StrCat(
+        (*path)[i]->letter_on_board(), " ", (*path)[i]->coords(), " -> ",
+        (*path).adjusted_points()[i], " --- (needs to drop ",
+        (*path)[i]->row() - (*path).adjusted_points()[i].row, " rows)");
+  }
+}
+
 }  // namespace
+
+//------
+// main
 
 int main(int argc, const char *argv[]) {
   absl::StatusOr<Solver> solver = LoadSolver();
@@ -57,29 +97,13 @@ int main(int argc, const char *argv[]) {
 
   if (absl::GetFlag(FLAGS_print_current_options)) {
     solver->FillWordCache();
-    auto wc = solver->word_cache();
-    LOG(INFO) << "All possible words on grid: ";
-    for (const auto &[score, wds] : wc) {
-      LOG(INFO) << absl::StrCat(
-          score, ": ",
-          absl::StrJoin(wds, ", ", [](std::string *out, const Path &path) {
-            absl::StrAppend(out, path.word());
-          }));
-    }
+    PrintCurrentOptions(*solver);
   }
 
   if (absl::GetFlag(FLAGS_print_longest_allstar_word)) {
-    absl::StatusOr<Path> path = solver->LongestPossibleAllStarWord();
-    if (!path.ok()) {
-      LOG(ERROR) << path.status();
+    if (absl::Status s = PrintLongestAllStarWord(*solver); !s.ok()) {
+      LOG(ERROR) << s;
       return 1;
-    }
-    LOG(INFO) << absl::StrCat("Longest possible all-star word: ", path->word());
-    for (int i = 0; i < path->size(); ++i) {
-      LOG(INFO) << absl::StrCat(
-          (*path)[i]->letter_on_board(), " ", (*path)[i]->coords(), " -> ",
-          (*path).adjusted_points()[i], " --- (needs to drop ",
-          (*path)[i]->row() - (*path).adjusted_points()[i].row, " rows)");
     }
   }
 
